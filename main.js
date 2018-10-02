273808c4f2ca6ba99fc774d259589c7caf1d406d
Uri Rahimi <urahimi@usc.edu>
2018-10-01 17:04:03 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let RestaurantsTemplateString = document.getElementById("restaurant-template").innerHTML;
+let renderRestaurants = Handlebars.compile(RestaurantsTemplateString);
+
+let promise = $.ajax({
+  type: "get",
+  url: "https://thejsguy.com/teaching/2018/api/restaurants.json"
+});
+
+promise.then(restaurants => {
+  console.log(restaurants.data);
+  let renderedRestaurants = renderRestaurants({
+    restaurants: restaurants.data
+  });
+  $("body").append(renderedRestaurants);
+});
